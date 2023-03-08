a083c276162d7214d96c2b2f3a2d5504add10c35
Reynaldo Aponte <playrey2010@gmail.com>
2023-03-08 13:28:37 -0800

    Initial commit

---
+++ src/main/java/com/example/hibernate/demo/entity/Course.java
@@ -0,0 +1,91 @@
+package com.example.hibernate.demo.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    // define fields and add annotations
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private List<Review> reviews;
+
+
+    // constructors
+
+    public Course() {
+    }
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+
+    // getters setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    // Convenience add method
+
+    public void addReview(Review review) {
+        if (reviews == null) {
+            reviews = new ArrayList<Review>();
+        }
+
+        reviews.add(review);
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
